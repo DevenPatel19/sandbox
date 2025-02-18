f3e728caa616bbdc3eeda6ae57671e6e6e4c0718
Deven Patel <122069890+DevenPatel19@users.noreply.github.com>
2025-02-18 06:19:11 -0800

    Completed primality with optimized solution and powerofTwo algos

--- 09 - primality.js
+++ 09 - primality.js
@@ -1,51 +1,88 @@
 /**
  * Primality
- * TODO: Problem
+ * * Problem
  * Given a natural number  'n', determine if the number is prime or not
  * 
- *  The Factorial of a non-negative integer 'n', denoted n!, is the product of all positive integers less
- *  than or equal to 'n'
- *  The factorial of zero is 1
+ *  A Prime number is a natural number greter than 1 that is not a product of two smaller natural numbers
  * 
- * factorial(4) = 24
- * factorial(5) = 120
+ * prime(4) = false (1*4 or 2*2 or 4*1)
+ * prime(5) = true (5*1 or 1*5)
  *
 */
 
-function factorial(n) {
-    let result =  1                        // Big O 1
-    for(let i=2; i <= n; i++){             // Big O N
-        result *= i
-       }
-       return result                       // Big O 1
+function isPrime(n) {
+    if(n < 2){                              // Big O = n
+        return false                        // big O = 1
     }
+    for (let i =2; i < n ; i++){            // Big O = n
+        if(n%i === 0){                      // Big O = n
+            return false                    // Big O = 1
+        }
+        return true                         // Big O = 1
+    }
+}
     
     
-    console.log(factorial(1))
-    console.log(factorial(1))
-    console.log(factorial(4))
-    console.log(factorial(5))
+    console.log(isPrime(1)) // false
+    console.log(isPrime(4)) // false
+    console.log(isPrime(5)) // true
     
     /**
      * 
-     * Steps to solve:
+     * *Steps to solve:
      * 1.Create function signature (function name(variables){})
      * 
-     * 2. Create a constant to hold to answer we will generate since it is a product of numbers,
-     * lets use a variable to carry over the previous product
+     * 2. Create an edge case test to ensure n is positive and greater than 1 
      * 
-     * 3. Since we need to generate more numbers and multiply them by the previous product, lets setup a 'jig'
-     *  with a 'For Loop' to progressively calc the value
-     *  3a. we will start with i=2  because  0 and 1 are pre calculated..
-     *  3b. we make sure we keep going as long as i <= n because that is the parameter give by the user and is inclusive
-     *  3c. we use i++ because each loop through we want the next number so we increment by 1
+     * 3. We want to then test to see if any numbers will divide into our given parameter of n,
+     *      so we setup a jig with a for loop
+     *  3a. starting with 2 and stopping just before n, we will step through each number 1 by 1 
      * 
-     * 4. During each loop through we will want to calc the numbers so we set the referential formula 
-     *      for the next factorial number in the sequence to multiply to the current product
+     * 4. During each loop through we will want to calc the numbers so we calc the numbers using
+     *      modulo operator to verify is the value is exactly zero. if it doesn't then false if it does true
      * 
-     * 5. Then we can return the answer.
+     * 5. We then add some test calls
      * 
-     * 6. We then add some test calls
-     * 
-     * 7. Determine Big O ---- based on side notes Big O = O(n) = Linear time
+     * 6. Determine Big O ----> based on side notes Big O = O(n) = Linear time
      */
+
+
+
+    /**
+     * *Optimized Primatlity Test
+     * Integers larger than the squaare root do not need to be checked because, whenever 'n=a*b', one of 
+     * the two factors 'a' and 'b' is less than or equal to the square root of 'n'
+     * 
+     * n=24, a=4 and b=6
+     * 
+     * The square root of 24 is 4.89
+     * a is less than the square root of n
+     * 
+     * a is less than the square root of n
+     * 
+     * n=35, a=5 and b=7
+     * 
+     * square root of 35 is 5.91
+     * 5 is less tan 5.91
+     * 
+     * * Big O Changes: 
+     * The change is that when 'n' is a 1000 we only check up to 100,
+     * this gives us a O(sqrt(n))
+     */
+
+    function isPrimeOptimized(n) {
+        if(n < 2){
+            return false
+        }
+        for(let i = 2; i <= Math.sqrt(n); i++){
+            if(n % i === 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    console.log(isPrimeOptimized(1) + " = false") // false
+    console.log(isPrimeOptimized(4) + " = false") // false
+    console.log(isPrimeOptimized(5) + " = true") // true
