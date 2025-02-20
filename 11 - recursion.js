56b386f326335448ea2be5f48773e81aa19ff645
Deven Patel <122069890+DevenPatel19@users.noreply.github.com>
2025-02-20 15:09:45 -0800

    Completed Recursion notes and Started fibSeqRec solution

---
+++ 11 - recursion.js
@@ -0,0 +1,11 @@
+/**
+ ** Recursion - a problem solving technique where the solution depends on solutions to smaller instances of the
+ **              same problem. aka - when a function calls itself
+ *
+ *! Find yourself breaking down your problem into smaller versions of the same problem 
+ *
+ *! Every resursive solution needs to have a base case - a condition to terminate the recursion.
+ *! Recurison may simplify solving a prblem BUT it does not always x-late t a faster solution. (could be worse).
+ *
+ *
+ */
