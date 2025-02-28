a0f9e1cc7633baf16e8c9c967db321cbfc005755
Deven Patel <122069890+DevenPatel19@users.noreply.github.com>
2025-02-28 14:45:50 -0800

    Completed generalized search algo notes

--- 14 - searcAlgorithms.js
+++ 14 - searcAlgorithms.js
@@ -1,5 +1,10 @@
 /**
  * *Search Algorithms = Linear Search & Binary Search as well as Recursive binary search
  * 
- * ! Problem statement: 
+ * ! Problem statement: There are 3 types of search algos:
+ * Linear search
+ * 
+ * Binary search
+ * 
+ * Recursive Binary search
  */
