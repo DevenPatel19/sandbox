1c36453ceb8c6d870b18188af7e814ed90377929
Deven Patel <122069890+DevenPatel19@users.noreply.github.com>
2025-03-03 12:01:11 -0800

    Completed Insertion Sort Quadratic time solution

--- 22 - 24 insertionSort.js
+++ 22 - 24 insertionSort.js
@@ -1,27 +1,33 @@
 /** INSERTION SORT
- * !Problem Statement: Given an arra, sorth the array.
- * 
+ * !Problem Statement: Given an array, sorth the array.
+ *
  * const arr = [-6, 20, 8, -2, 4]
- * 
+ *
  * *Concept: virtually split array into sorted and unsorted. Assume that 1st element is already sorted and remaining
  * *    are unsorted. Select an unsorted element and compare with all elelemnts in the sorted part. If elements in
  * *    the sorted part is smalled than the selected element, proceed to the next element in the unsorted part. Else,
- * *    shift larger elements in the sorted part towards the right. Insert the selected element at the right index. 
+ * *    shift larger elements in the sorted part towards the right. Insert the selected element at the right index.
  * *    Repeat until all elements are placed in the correct order.
- * 
+ *
  */
 
-
 function insertionSort(arr) {
-    for(let i=1; i = arr.length-1; i++){
-        if(arr[i]<arr[i-1]){
-           
-        } 
+  for (let i = 1; i < arr.length - 1; i++) {          // n
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {       // n
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
-    return arr
+    arr[j + 1] = numberToInsert;
+  }
 }
 
+const arr = [-6, 20, 8, -2, 4];
+insertionSort(arr);
+console.log(arr) / [-6, -2, 4, 8, 20];
 
-const arr = [ -6, 20, 8, -2, 4]
-insertionSort(arr)
-console.log(arr) / [ -6, -2, 4, 8, 20]
+
+/**
+ * Big O notation = O(n^2) = quadratic time complexity
+ */
